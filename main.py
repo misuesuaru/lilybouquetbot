840a76e2180a5ac7310375c5c1e27bcba22c61cd
misuesuaru <110294792+misuesuaru@users.noreply.github.com>
2026-03-04 10:45:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def home():
 
 def keep_alive():
     def run():
-        app.run(host='0.0.0.0', port=8080)
+        port = int(os.environ.get("PORT", 8080))
+        app.run(host='0.0.0.0', port=port)
     t = Thread(target=run)
     t.daemon = True
     t.start()
